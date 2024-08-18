f54f3ed2a4e71d3e24503691d2ad45680b90a7af
Dzmitry Branavitski <d.bronovitskiy@brimit.com>
2024-08-18 12:08:49 +0200

    update angular

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { MaterialModule } from './material.module';
 
-@NgModule({
-  declarations: [
-  ],
-  imports: [
-    MaterialModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-  ],
-  exports: [
-    MaterialModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-  ]
-})
+@NgModule({ declarations: [],
+    exports: [
+        MaterialModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+    ], imports: [MaterialModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class SharedModule { }
